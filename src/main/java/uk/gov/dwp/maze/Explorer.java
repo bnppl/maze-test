b302bff423e43961cf1318114703c2fdfa63249a
Ben Eppel <b.eppel@gmail.com>
2016-10-04 16:20:55 +0100

    Initial commit
    Add initial project files for test.

---
+++ src/main/java/uk/gov/dwp/maze/Explorer.java
@@ -0,0 +1,19 @@
+package uk.gov.dwp.maze;
+
+public class Explorer {
+
+    private int[] movement;
+
+    public void exploreMaze(Maze m){
+
+    }
+
+    private void move(){
+
+    }
+
+    private void turn(){
+
+    }
+
+}
